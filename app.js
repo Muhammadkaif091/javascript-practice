ab1581b858b13efff4dcbd988e822ccb02a4aea4
Muhammadkaif091 <86207223+Muhammadkaif091@users.noreply.github.com>
2024-02-22 21:20:37 +0500

    javascript practice

---
+++ app.js
@@ -0,0 +1,7 @@
+var fristNumber = Number(prompt("enter your frist number"));
+var lastNumber = Number(prompt("enter your last number"));
+
+alert(" additional result :" + (fristNumber + lastNumber) );
+alert(" Subtraction result :" + (fristNumber - lastNumber) );
+alert(" multiplication result :" + (fristNumber * lastNumber) );
+alert(" division result :" + (fristNumber / lastNumber) );
